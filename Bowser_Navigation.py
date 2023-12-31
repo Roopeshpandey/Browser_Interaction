383856e20a9c7e0939896c0204f74a92e6a07556
Roopesh pandey <roopeshpan1009@gmail.com>
2023-12-31 13:08:33 +0530

    Browser naviagation file add

---
+++ Bowser_Navigation.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://the-internet.herokuapp.com/")
+driver.find_element(By.XPATH,"//a[text()='A/B Testing']").click()
+driver.refresh()
+time.sleep(2)
+driver.back()
+time.sleep(2)
+driver.forward()
+
+
+print(f"Successfull :{driver.title} ")
